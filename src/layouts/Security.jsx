f40e72c8a7bbe6c6f8af9ad1e47bc0059770419d
Alexis Amadei <alexisamadei@gmail.com>
2024-04-23 17:43:41 +0200

    [UPDATE] Mise à jour de la page de sécurité

--- src/layouts/Security.jsx
+++ src/layouts/Security.jsx
@@ -1,12 +1,8 @@
-import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-export default function Security({ user }) {
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (user) { navigate("/"); }
-    }, [user, navigate]);
-    console.log('user', user);
-    if (user) return null;
-    return <Outlet />;
+export default function Security() {
+    return (
+        <Outlet />
+    )
 }
