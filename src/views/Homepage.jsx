fcc4ad5e3181dce25ed130fb3bdd205368bf625c
Alexis Amadei <alexisamadei@gmail.com>
2024-05-01 13:57:47 +0200

    [UPDATE] fix disabled button and create route to selected match

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -3,40 +3,50 @@ import { NewMatch } from "../services/NewMatch";
 import { getMatches } from "../services/getMatches";
 import { LuSwords } from "react-icons/lu";
 import './styles/HomePage.css';
+import { useNavigate } from "react-router-dom";
 
 export default function Homepage() {
   const [userConnected, setUserConnected] = useState({ token: '', username: '' });
   const [matchList, setMatchList] = useState([]);
   const [allowCreateMatch, setAllowCreateMatch] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('userConnected'));
+    setUserConnected(user);
+    handleRefresh();
+  }, []);
 
   function handleRefresh() {
     getMatches().then((matches) => {
       setMatchList(matches);
     });
   }
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('userConnected'));
-    setUserConnected(user);
-  }, []);
-  const handleNewMatchClick = () => {
-    NewMatch();
-  };
+
   useEffect(() => {
     matchList.forEach(match => {
-      if (match.user2 === '') {
+      if (match.user2 === null) {
         setAllowCreateMatch(false);
-      } else {
-        setAllowCreateMatch(true);
       }
     });
   }, [matchList])
+
+  function handleNewMatchClick() {
+    NewMatch();
+    handleRefresh();
+  };
+
+  function handleMatchSelect(matchId) {
+    navigate(`/private/match/${matchId}`);
+  }
+
   return (
     <div className="homepage-wrapper">
       <p>Welcome <span>{userConnected.username}</span> !</p>
       <div className="game-buttons">
         <button onClick={handleRefresh}>Refresh matches</button>
         <button
-          disabled={allowCreateMatch ? true : false}
+          disabled={!allowCreateMatch}
           onClick={handleNewMatchClick}
         >New Game</button>
       </div>
@@ -46,7 +56,7 @@ export default function Homepage() {
         )}
         {matchList.length > 0 && (
           matchList.map((match) => (
-            <div key={match._id} className="match-item">
+            <div key={match._id} className="match-item" onClick={() => handleMatchSelect(match._id)}>
               <p style={{ textAlign:'center'}}>Match ID: {match._id}</p>
               <div className="players">
                 <p>Player 1: {match.user1.username}</p>
