0722b9a95d97afb597de662bfeb3057515c0e9f9
Alexis Amadei <alexisamadei@gmail.com>
2024-05-09 14:58:44 +0200

    [UPDATE] error handling when fail to fetch

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,38 +1,69 @@
 import React, { useContext, useState } from 'react'
 import { AuthContext } from '../contexts/Auth'
 import { useNavigate } from 'react-router-dom'
-import Homepage from './Homepage'
+import CircularProgress from '@mui/material/CircularProgress';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import ReplayIcon from '@mui/icons-material/Replay';
+import './styles/Login.css';
 
 export default function Login() {
-  const { userConnected, actions } = useContext(AuthContext);
-  const [error, setError] = useState(null);
+  const { actions } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isError, setIsError] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const username = e.target.username.value
     const password = e.target.password.value
 
-
-    console.log(username, password)
-
+    setLoading(true)
     try {
       await actions.loginUser({ username, password });
-      navigate('/private/homepage')
-      setError(null)
+      setError(null);
+      navigate('/private/homepage');
     } catch (error) {
-      setError(error.message)
+      setIsError(true);
+      setLoading(false);
+      setError(error.message);
     }
+  }
+
+  function handleReload(e) {
+    e.preventDefault();
+    setIsError(false);
+    setError(null);
+    setLoading(false);
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type='text' name='username' placeholder='username' />
-        <input type='password' name='password' placeholder='password' />
-        <button type='submit'>Login</button>
-      </form>
-      {error && <p>{error}</p>}
+      {loading && (
+        <CircularProgress
+          color='secondary'
+          size={50}
+        />
+      )}
+      {isError && (
+        <div className='error-display'>
+          <ErrorOutlineIcon
+            color='error'
+            fontSize='large'
+          />
+          {error && <p>{error}</p>}
+          <p id='try-again' onClick={handleReload}>Try again
+            <ReplayIcon />
+          </p>
+        </div>
+      )}
+      {!loading && !isError && (
+        <form onSubmit={handleSubmit}>
+          <input type='text' name='username' placeholder='username' />
+          <input type='password' name='password' placeholder='password' />
+          <button type='submit'>Login</button>
+        </form>
+      )}
     </div>
   )
 }
